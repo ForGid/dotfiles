103f0e3e862f8d35b6cc9774c056b7b7325c8f31
Forgács Dávid <david@gastrosol.hu>
2025-06-30 17:40:09 +0200

    v1 maybe

--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
 
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
@@ -75,7 +75,6 @@ plugins=(git)
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
-export PATH="$HOME/.local/bin:$PATH"
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
@@ -108,3 +107,6 @@ fi
 
 alias vi='nvim'
 alias vim='nvim'
+alias code='nvim'
+alias nano='nvim'
+alias emacs="nvim"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,68 @@
 #!/bin/sh
 
-echo "[dotfiles] Installing packages..."
+cd ~ || exit 1
+
+echo "[dotfiles] Updating system..."
+sudo pacman -Syu --needed --noconfirm
+
+echo "[dotfiles] Installing official packages..."
 sudo pacman -S --needed --noconfirm $(grep -vE '^\s*#' ~/dotfiles/packages.txt)
+
+# yay install
+if ! command -v yay >/dev/null; then
+  echo "[dotfiles] Installing yay..."
+  git clone https://aur.archlinux.org/yay.git ~/yay
+  cd ~/yay || exit 1
+  makepkg -si --noconfirm
+  cd ~ && rm -rf ~/yay
+else
+  echo "[dotfiles] yay already installed -- skipping"
+fi
+
+echo "[dotfiles] Installing AUR packages..."
+yay -S --needed --noconfirm $(grep -vE '^\s*#' ~/dotfiles/aur.txt)
 
 echo "[dotfiles] Bootstrapping configs..."
 
-# Ensure ~/.config exists
 mkdir -p ~/.config
 
 echo "[dotfiles] Linking configs..."
-ln -srf ~/dotfiles/.config/nvim ~/.config/
-ln -srf ~/dotfiles/.config/hypr ~/.config/
-ln -srf ~/dotfiles/.config/kitty ~/.config/
-ln -srf ~/dotfiles/.config/fastfetch ~/.config/
-ln -srf ~/dotfiles/.config/waybar ~/.config/
+CONFIG_DIRS=(nvim hypr kitty fastfetch waybar)
+
+for dir in "${CONFIG_DIRS[@]}"; do
+  target="$HOME/.config/$dir"
+  src="$HOME/dotfiles/.config/$dir"
+
+  if [ -d "$target" ] && [ ! -L "$target" ]; then
+    echo "[dotfiles] Removing real directory at $target"
+    rm -rf "$target"
+  fi
+
+  ln -srf "$src" "$target"
+done
+
 ln -srf ~/dotfiles/.zshrc ~/
+ln -srf ~/dotfiles/.gitconfig ~/
 ln -srf ~/dotfiles/.zprofile ~/
-ln -srf ~/dotfiles/screen_saver.sh ~/
-chmod +x ~/screen_saver.sh
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "[dotfiles] Cloning Oh My Zsh..."
+  git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+else
+  echo "[dotfiles] Oh My Zsh already present — skipping"
+fi
 
 mkdir -p ~/.local/bin
-ln -srf ~/dotfiles/setwall ~/.local/bin/
+
+echo "[dotfiles] Linking custom scripts..."
+ln -srf ~/dotfiles/scripts/screen_saver.sh ~/.local/bin/
+chmod +x ~/.local/bin/screen_saver.sh
+
+ln -srf ~/dotfiles/scripts/setwall ~/.local/bin/
 chmod +x ~/.local/bin/setwall
 
-echo "[dotfiles] Done."
+echo "[dotfiles] Linking wallpapers"
+mkdir -p ~/Pictures
+ln -srf ~/dotfiles/wallpapers ~/Pictures/wallpapers
+
+fastfetch
